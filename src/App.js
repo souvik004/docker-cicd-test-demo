c5255a673b7596c8c207734281da3acba1909e31
sp004 <souvikpal4049@gmail.com>
2023-08-13 11:13:02 +0530

    adding Test component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Form from './components/Form';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className='app'>
+      <Test />
       <Form />
     </div>
   );
